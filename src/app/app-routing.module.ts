22ff3fa7f17a68f1713457944527dccd84048ef5
Lucone00 <luca.giuntoli2001.b@gmail.com>
2024-02-19 11:30:22 +0100

    first component with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './features/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: WelcomePageComponent },
+  {
+    path: 'welcome',
+    component: WelcomePageComponent,
+    loadChildren: () =>
+      import('./features/welcome-page/welcome-page.module').then(
+        (m) => m.WelcomePageModule
+      ),
+  },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { StoreModule } from '@ngrx/store';
+import { SharedModule } from './shared/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    SharedModule,
+    StoreModule.forRoot({}, {}),
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
