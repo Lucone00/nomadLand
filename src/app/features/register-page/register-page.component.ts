af29c1cf08b4c4d77188d6752ed2627e1233468c
Lucone00 <luca.giuntoli2001.b@gmail.com>
2024-03-06 12:30:01 +0000

    aggiunti controlli sul form

--- src/app/features/register-page/register-page.component.ts
+++ src/app/features/register-page/register-page.component.ts
@@ -4,24 +4,53 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.scss',
+  styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent {
   registrationForm!: FormGroup;
   currentStep = 1;
+  isNextButtonDisabled = false;
+  isCurrentFieldValid = false;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.registrationForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      username: ['', Validators.required],
+      username: ['', [Validators.required, Validators.pattern(/^.{3,14}$/)]],
       lastTrip: ['', Validators.required],
     });
+
+    this.updateButtonState();
+
+    this.registrationForm.valueChanges.subscribe(() => {
+      this.updateButtonState();
+    });
+  }
+
+  updateButtonState() {
+    const currentField = this.getFieldByStep(this.currentStep);
+    this.isCurrentFieldValid = currentField ? currentField.valid : false;
+    this.isNextButtonDisabled =
+      this.registrationForm.invalid || this.registrationForm.pending;
+  }
+
+  getFieldByStep(step: number) {
+    switch (step) {
+      case 1:
+        return this.registrationForm.get('email');
+      case 2:
+        return this.registrationForm.get('username');
+      case 3:
+        return this.registrationForm.get('lastTrip');
+      default:
+        return null;
+    }
   }
 
   nextStep() {
     this.currentStep++;
+    this.updateButtonState();
   }
 
   onSubmit() {
